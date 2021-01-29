98f2e97628ede3f5586a83aa580f84db614dcb89
Anthony H <huckle.anthony@outlook.com>
2021-01-29 17:44:53 +0100

    Create TennisCoach class
    Create TennisCoach class the implements the Coach interface.

---
+++ src/com/springandhibernateforbeginners/exercises/project02/TennisCoach.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.springandhibernateforbeginners.exercises.project02;
+
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author anthu
+ */
+@Component("thatSillyCoach")
+public class TennisCoach implements Coach {
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice your backhand volley";
+    }
+    
+}
